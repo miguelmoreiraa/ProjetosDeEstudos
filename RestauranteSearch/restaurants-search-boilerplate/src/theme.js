29fb8e7dd905f6942b2f0e155514df42b3fef639
miguelmoreiraa <miguel.moreira.mm44@gmail.com>
2021-10-05 19:40:42 -0300

    :art: Cria home page e reset dos estilos

---
+++ RestauranteSearch/restaurants-search-boilerplate/src/theme.js
@@ -0,0 +1,13 @@
+const theme = {
+    colors: {
+      primary: '#62000ee',
+      background: '#0000014',
+      text: '000000B3',
+      sucess: '#135E01',
+    },
+    fonts: {
+      regular: 'Roboto, sans-serif',
+    },
+};
+
+export default theme;
